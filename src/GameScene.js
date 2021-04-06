025d557d4e112bbe80eb9e02094965e206d025f9
gomezvt <greg@gomez.ws>
2021-04-06 00:14:48 -0400

    Set SpriteSheet resizeMode to contain

--- src/GameScene.js
+++ src/GameScene.js
@@ -10,31 +10,27 @@ import {
   ImageBackground,
   Dimensions,
 } from 'react-native';
-import { WarriorIdle, WarriorAttack } from "./heroes/Warrior";
+import { Warrior } from "./heroes/Warrior";
 import { GameEngine, GameLoop } from 'react-native-game-engine';
 
 const { height } = Dimensions.get('window')
 const { width } = Dimensions.get('window')
 const pauseButton = require('../img/pause.png');
 const menuButton = require('../img/menu.png');
-const gamepad = require('../img/gamepad.png');
+// const gamepad = require('../img/gamepad.png');
 
 export default class GameScene extends PureComponent {
   constructor(props) {
     super(props);
     this.state = {
       hero: 'warrior',
-      attacked: false,
+      type: 'idle',
       x: -width / 3,
       y: height / 2,
     };
   }
 
   updateHandler = ({ touches, screen, layout, time }) => {
-    let press = touches.find(x => x.type === "press");
-    if (press) {
-      const p = press;
-    }
 
     // let move = touches.find(x => x.type === "move");
     // if (move) {
@@ -44,16 +40,18 @@ export default class GameScene extends PureComponent {
     //   });
     // }
 
-    // const { attacked } = this.state;
-    // let press = touches.find(x => x.type === "press");
-    // if (press) {
-    //   this.setState({ attacked: true });
-    // } else if (attacked) {
-    //   setTimeout(() => { this.setState({ attacked: false }) }, 300);
-    // }
+    let press = touches.find(x => x.type === "press");
+    if (press) {
+      this.setState({
+        type: 'attack'
+      }, () => {
+        setTimeout(() => { this.setState({ type: 'idle' }) }, 300)
+      });
+    }
   };
 
   render() {
+    const { type } = this.state;
     return (
       <>
         <View style={{ width: "100%", flexDirection: 'row', justifyContent: 'space-evenly' }}>
@@ -69,7 +67,7 @@ export default class GameScene extends PureComponent {
           </TouchableOpacity>
         </View>
         <GameLoop style={styles.gameContainer} onUpdate={this.updateHandler}>
-          {this.state.attacked ? <WarriorAttack /> : <WarriorIdle />}
+          <Warrior type={type} loop={true} resetAfterFinish={false} />
           {/* <TouchableOpacity
             onPress={() => { }}
             style={{ left: 25, top: -50 }}
--- src/heroes/Warrior.js
+++ src/heroes/Warrior.js
@@ -5,110 +5,63 @@ import SpriteSheet from 'rn-sprite-sheet';
 const { height } = Dimensions.get('window')
 const { width } = Dimensions.get('window')
 
-export class WarriorIdle extends PureComponent {
+export class Warrior extends PureComponent {
   constructor(props) {
     super(props);
     this.state = {
-      loop: true,
-      resetAfterFinish: false,
-      fps: '16',
-      hero: 'warrior',
+      loop: props.loop,
+      resetAfterFinish: props.resetAfterFinish,
+      fps: 16,
       x: -width / 3,
       y: height / 3,
     };
   }
 
   componentDidMount() {
-    this.play('idle');
+    this.play(this.props.type);
   }
 
   play = type => {
     const { fps, loop, resetAfterFinish } = this.state;
-
-    if (this.warrior) {
-      this.warrior.play({
-        type,
-        fps: Number(fps),
-        loop: loop,
-        resetAfterFinish: resetAfterFinish,
-        onFinish: () => console.log('hi')
-      });
-    }
+    this.warrior.play({
+      type,
+      fps: Number(fps),
+      loop: loop,
+      resetAfterFinish: resetAfterFinish,
+      onFinish: () => console.log('finished')
+    });
   };
 
   stop = () => {
     this.warrior.stop(() => console.log('stopped'));
   };
 
-  render() {
-    const { x, y } = this.state;
-    return (
-      // <View style={{ left: x, top: y }}>
-        <SpriteSheet
-          ref={ref => (this.warrior = ref)}
-          source={require('../../sprites/warrior/Idle.png')}
-          columns={10}
-          rows={1}
-          width={225}
-          animations={{
-            idle: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
-          }}
-        />
-      // </View>
-    );
-  }
-}
-
-export class WarriorAttack extends PureComponent {
-  constructor(props) {
-    super(props);
-    this.state = {
-      loop: false,
-      resetAfterFinish: true,
-      fps: '16',
-      hero: 'warrior',
-      x: -width / 3,
-      y: height / 3,
-    };
-  }
-
-  componentDidMount() {
-    this.play('attack');
-  }
-
-  play = type => {
-    const { fps, loop, resetAfterFinish } = this.state;
-
-    if (this.warrior) {
-      this.warrior.play({
-        type,
-        fps: Number(fps),
-        loop: loop,
-        resetAfterFinish: resetAfterFinish,
-        onFinish: () => console.log('hi')
-      });
+  getSpriteData = () => {
+    switch (this.props.type) {
+      case 'idle':
+        return { source: require('../../sprites/warrior/Idle.png'), cols: 10, width: 250 }
+      case 'attack':
+        return { source: require('../../sprites/warrior/Attack1.png'), cols: 4, width: 250 }
+      default:
+        break;
     }
-  };
-
-  stop = () => {
-    this.warrior.stop(() => console.log('stopped'));
-  };
+  }
 
   render() {
-    const { x, y } = this.state;
+    const data = this.getSpriteData();
     return (
-      // <View style={{ left: x, top: y }}>
-        <SpriteSheet
-          ref={ref => (this.warrior = ref)}
-          source={require('../../sprites/warrior/Attack1.png')}
-          columns={4}
-          rows={1}
-          width={225}
-          animations={{
-            attack: [0, 1, 2, 3],
-          }}
-        />
-      // </View>
+      <SpriteSheet
+        ref={ref => (this.warrior = ref)}
+        source={data.source}
+        columns={data.cols}
+        rows={1}
+        width={data.width}
+        imageStyle={{resizeMode: 'contain'}}
+        animations={{
+          idle: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+          attack: [0, 1, 2, 3],
+        }}
+      />
     );
   }
 }
